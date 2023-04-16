b77cfe5bd41dd3053edcce50dcbfc5a80795a659
nikvoid <77514082+nikvoid@users.noreply.github.com>
2023-04-16 18:19:04 +0300

    display associated elements

--- src/view/element.rs
+++ src/view/element.rs
@@ -1,13 +1,30 @@
 use actix_web::{Responder, get, web, error::{ErrorNotFound, ErrorInternalServerError}};
-use maud::{Render, html};
+use maud::{Render, html, html_to};
 use tracing::error;
 
-use crate::{dao::{STORAGE, ElementStorage}, view::{BaseContainer, AsideTags, ElementLink, TagEditForm, AsideMetadata, ScriptButton}};
+use crate::{dao::{STORAGE, ElementStorage}, view::{BaseContainer, AsideTags, ElementLink, TagEditForm, AsideMetadata, ScriptButton}, model::read::Element, resolve};
+
+struct ElementGroup<'a>(&'a [Element]);
+impl Render for ElementGroup<'_> {
+    fn render_to(&self, buffer: &mut String) {
+        html_to! { buffer,
+            @for e in self.0 {
+                .image-container-list {
+                    a href=(resolve!(/element/e.id)) {
+                        img.def-img.image-list-element 
+                            // TODO: Thumbnail
+                            src=(ElementLink(e)) alt="image";
+                    }
+                }
+            }
+        }
+    }
+}
 
 #[get("/element/{id}")]
 pub async fn element_page(id: web::Path<u32>) -> impl Responder {
     let id = *id;
-    
+
     let (elem, meta) = match STORAGE
         .lock()
         .await
@@ -19,6 +36,27 @@ pub async fn element_page(id: web::Path<u32>) -> impl Responder {
                 return Err(ErrorInternalServerError("failed to fetch element data"));
             }
         };
+    
+    let associated = match elem.group_id {
+        Some(group_id) => { 
+            let res = STORAGE
+                .lock()
+                .await
+                .search_elements(format!("group:{}", group_id), 0, 200, 0)
+                .map(|(res, ..)| res);
+            
+            Some(res)
+        }
+        None => None,
+    };
+
+    let associated = match associated.transpose() {
+        Ok(elems) => elems,
+        Err(e) => {
+            error!(?e, "failed to fetch element group");
+            None
+        }
+    };
     
     let page = BaseContainer {
         after_header: match elem.animated {
@@ -44,13 +82,15 @@ pub async fn element_page(id: web::Path<u32>) -> impl Responder {
                     }
                 }
             } 
+            @if let Some(elems) = associated {
+                .index-side { (ElementGroup(&elems)) }
+            }
         }),
         aside: Some(html! {
             (AsideTags(&meta.tags))
             (AsideMetadata(&meta))
             // TODO: Replace GET-query based updates with typescript onclicks
             (TagEditForm("", "add_tag", "Add tag"))
-            // TODO: Element group
         }),
         ..Default::default()
     };
