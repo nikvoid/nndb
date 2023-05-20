a1f945e4edc55307a9a3f82eb4955655fc64eb32
nikvoid <77514082+nikvoid@users.noreply.github.com>
2023-05-20 15:09:05 +0300

    rewrite add_elements() to sqlx

--- src/dao/sqlitex.rs
+++ src/dao/sqlitex.rs
@@ -1,12 +1,195 @@
-use sqlx::{SqlitePool, migrate::MigrateDatabase};
+use std::path::PathBuf;
 
-use crate::model::{write::{self, ElementWithMetadata}, read::{self, PendingImport}, Summary, Md5Hash, GroupMetadata};
+use sqlx::Executor;
+use sqlx::{SqlitePool, migrate::MigrateDatabase, SqliteConnection};
+
+use crate::util;
+use crate::{model::{write::{self, ElementWithMetadata}, read::{self, PendingImport}, Summary, Md5Hash, GroupMetadata, AIMetadata}, config::CONFIG};
+use futures::StreamExt;
+
+use tracing::error;
 
 pub struct Sqlite(SqlitePool);
 
 // TEMP:
 pub type StorageError = anyhow::Error;
 
+/// Private methods and associated functions
+impl Sqlite {
+    async fn get_tags_hashes_tx(
+        tx: &mut SqliteConnection
+    ) -> Result<Vec<u32>, StorageError> {
+        let hashes = sqlx::query_scalar!(
+            r#"SELECT name_hash as "h: u32" FROM tag"#
+        )
+        .fetch_all(tx)
+        .await?;
+
+        Ok(hashes)
+    }
+
+    async fn add_ai_metadata_tx(
+        tx: &mut SqliteConnection,
+        element_id: u32, 
+        ai_meta: &AIMetadata
+    ) -> Result<(), StorageError> {
+        sqlx::query!(
+            "INSERT INTO ai_metadata 
+            (element_id, positive_prompt, negative_prompt, steps, scale,
+            sampler, seed, strength, noise)
+            VALUES 
+            (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            element_id, 
+            ai_meta.positive_prompt,
+            ai_meta.negative_prompt,
+            ai_meta.steps,
+            ai_meta.scale,
+            ai_meta.sampler,
+            ai_meta.seed,
+            ai_meta.strength,
+            ai_meta.noise
+        )
+        .execute(tx)
+        .await?;
+
+        Ok(())
+    }
+
+    async fn add_element_tx(
+        tx: &mut SqliteConnection, 
+        element: &ElementWithMetadata
+    ) -> Result<u32, StorageError> {
+        let ElementWithMetadata(e, m) = element; 
+
+        let hash = e.hash.as_slice();
+        let id = sqlx::query!(
+            "INSERT INTO element (filename, orig_name, hash, broken, animated)
+            VALUES (?, ?, ?, ?, ?)",
+            e.filename,
+            e.orig_filename,
+            hash,
+            e.broken,
+            e.animated
+        )
+        .execute(&mut *tx)
+        .await?
+        .last_insert_rowid();
+        
+        match m {
+            // Add metadata right here
+            Some(meta) => {
+                Self::add_metadata_tx(tx, id as u32, meta).await?;
+            },
+            // Insert import row
+            None => {
+                let imp_id: u8 = e.importer_id.into();
+                sqlx::query!(
+                    "INSERT INTO import (element_id, importer_id) VALUES (?, ?)",
+                    id,
+                    imp_id
+                )
+                .execute(&mut *tx)
+                .await?;
+            },
+        }
+
+        if let Some(sig) = e.signature {
+            let sig = bytemuck::cast_slice(&sig);
+            sqlx::query!(
+                "INSERT INTO group_metadata (element_id, signature) VALUES (?, ?)",
+                id,
+                sig
+            )
+            .execute(tx)
+            .await?;
+        }
+        
+        Ok(id as u32)
+    }
+    
+    // FIXME:
+    async fn add_metadata_tx(
+        tx: &mut SqliteConnection,
+        element_id: u32, 
+        meta: &write::ElementMetadata
+    ) -> Result<(), StorageError> {
+
+        if !meta.tags.is_empty() {
+            Self::add_tags_tx(tx, Some(element_id), &meta.tags).await?;
+        }
+
+        sqlx::query!(
+            "DELETE FROM import WHERE element_id = ?",
+            element_id
+        )
+        .execute(&mut *tx)
+        .await?;
+        
+        sqlx::query!(
+            "INSERT INTO metadata (element_id, src_link, src_time, ext_group)
+            VALUES (?, ?, ?, ?)",
+            element_id, 
+            meta.src_link,
+            meta.src_time,
+            meta.group
+        )
+        .execute(&mut *tx)
+        .await?;
+        
+        if let Some(ai) = &meta.ai_meta {
+            Self::add_ai_metadata_tx(tx, element_id, ai).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn add_tags_tx<T>(
+        tx: &mut SqliteConnection,
+        element_id: Option<u32>, 
+        tags: &[T]
+    ) -> Result<(), StorageError> 
+    where T: AsRef<write::Tag> {    
+        let hashes = Self::get_tags_hashes_tx(&mut *tx).await?;
+    
+        for t in tags {
+            let t = t.as_ref();
+            let hash = t.name_hash();
+            let name = t.name();
+            let alt_name = t.alt_name();
+            let typ: u8 = t.tag_type().into();
+
+            if !hashes.contains(&hash) {
+                sqlx::query!(
+                    "INSERT INTO tag (name_hash, tag_name, alt_name, tag_type)
+                    VALUES (?, ?, ?, ?)
+                    ON CONFLICT (name_hash) DO NOTHING",
+                    hash,
+                    name,
+                    alt_name,
+                    typ
+                )
+                .execute(&mut *tx)
+                .await?;
+            }
+            
+            if let Some(id) = element_id {
+                sqlx::query!(
+                    "INSERT INTO element_tag (element_id, tag_hash)                 
+                    VALUES (?, ?)
+                    ON CONFLICT (element_id, tag_hash) DO NOTHING",
+                    id, 
+                    hash
+                )
+                .execute(&mut *tx)
+                .await?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// Public API
 impl Sqlite {
     /// Connect to url and init storage
     pub async fn init(url: &str) -> Result<Self, StorageError> {
@@ -24,18 +207,85 @@ impl Sqlite {
     /// Returns count of new elements
     pub async fn add_elements<E>(&self, elements: &[E]) -> Result<u32, StorageError>
     where E: AsRef<ElementWithMetadata> {
-        Ok(0)
+        let mut hashes = self.get_hashes().await?;
+        let mut o_path = PathBuf::from(&CONFIG.element_pool);
+        let mut count = 0;
+        
+        for elem in elements {
+            let ElementWithMetadata(e, _) = elem.as_ref();
+            
+            // Deduplication
+            match (hashes.contains(&e.hash), &CONFIG.testing_mode) {
+                (true, true) => continue,
+                (true, false) => {
+                    std::fs::remove_file(&e.path).ok();
+                },
+                _ => ()
+            };
+            
+            let mut tx = self.0.begin().await?;
+        
+            o_path.push(&e.filename);
+            
+            let id = match Self::add_element_tx(&mut tx, elem.as_ref()).await {
+                Ok(id) => Some(id),
+                Err(err) => {
+                    error!(?err, name=e.orig_filename, "failed to add element");
+                    None
+                },
+            }; 
+        
+            // Move or copy elements
+            if let Err(err) = if CONFIG.testing_mode {
+                std::fs::copy(&e.path, &o_path).map(|_| ())
+            } else {
+                std::fs::rename(&e.path, &o_path)
+            } {
+                error!(?err, name=e.orig_filename, "failed to move file"); 
+            }; 
+            
+            o_path.pop();
+
+            // Add tags derived from path to file
+            if let Some(id) = id {
+                let tags = util::get_tags_from_path(&e.path);
+                if !tags.is_empty() {
+                    Self::add_tags_tx(&mut tx, Some(id), tags.as_slice()).await?;
+                }
+            }
+            
+            tx.commit().await?;
+            // Add recently inserted hash
+            hashes.push(e.hash);
+
+            count += 1;
+        }
+ 
+        Ok(count)
     }
 
     /// Get all files' hashes
     pub async fn get_hashes(&self) -> Result<Vec<Md5Hash>, StorageError> {
-        Ok(vec![])
+        let hashes = sqlx::query!(
+            "SELECT hash FROM element"
+        )
+        .fetch_all(&self.0).await?
+        .into_iter()
+        .map(|anon| Md5Hash::try_from(anon.hash).unwrap())
+        .collect();
+        
+        Ok(hashes)
     }
 
     /// Add all tags from slice
-    pub async fn add_tags<T>(&self, element_id: Option<u32>, tags: &[T]) -> Result<(), StorageError>
+    pub async fn add_tags<T>(
+        &self, 
+        element_id: Option<u32>, 
+        tags: &[T]
+    ) -> Result<(), StorageError>
     where T: AsRef<write::Tag> {
-        Ok(())
+        let mut conn = self.0.acquire().await?;
+        Self::add_tags_tx(&mut conn, element_id, tags).await
     }
 
     /// Get all elements waiting on metadata
@@ -46,7 +296,9 @@ impl Sqlite {
     /// Add metadata for element -- and remove pending import
     pub async fn add_metadata<M>(&self, element_id: u32, metadata: M) -> Result<(), StorageError>
     where M: AsRef<write::ElementMetadata> + Send {
-        Ok(())
+        let m = metadata.as_ref();
+        let mut conn = self.0.acquire().await?;
+        Self::add_metadata_tx(&mut conn, element_id, m).await
     }
 
     /// Get all image signature groups stored in db
